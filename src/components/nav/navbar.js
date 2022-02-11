267cfe517c338f1c4365ff72cfc4a806290244c8
Isaac Weiser <isaac.weiser.x@gmail.com>
2022-02-11 10:46:26 -0500

    fixed nav menu

--- src/components/nav/navbar.js
+++ src/components/nav/navbar.js
@@ -22,75 +22,7 @@ import { IconButton } from "@mui/material";
 
 export const NavBar = () => {
 
-   /* const history= useHistory()
-
-    //this wil; handle the logic for the routing of the select options
-    const route=(event)=>
-    {
-        const option = parseInt(event.target.value)
-        console.log("opt "+option)
-        if (option > 0 && option <4)
-        {
-            if(option === 1)
-            {
-                history.push(`/`)
-                document.querySelector("#dropdown").value=0
-            }
-            else if(option === 2)
-            {
-                history.push(`/profile`)
-                document.querySelector("#dropdown").value=0
-            }
-            else if (option === 3)
-            {
-                history.push(`/favorites`)
-                document.querySelector("#dropdown").value=0
-            }
-            else if (option === 4)
-            {
-                window.localStorage.removeItem("zen_user")
-                history.push('/')
-                document.querySelector("#dropdown").value=0
-            }
-        }
-    }
-
-    return(
-        <ul className="navbar">
-            <li className="navbar__item active">
-                <Link  className="navbar__link" to="/about">
-                    About
-                </Link>
-            </li>
-            <li className="navbar__item active">
-                <Link  className="navbar__link" to="/profile">
-                    <img id="profile--img" src="background/ZEN_ref.png"></img>
-                </Link>
-            </li>
-            <li className="navbar__item active">
-                    <select id="dropdown" onChange={(evt)=>{route(evt)}}>
-                        <option id="profile-options" value="0">
-                            Select an option
-                        </option>
-                        <option id="option--profile" className="profile-options" value="1">
-                            Home
-                        </option>
-                        <option id="option--favorite" className="profile-options" value="2">
-                        Profile
-                        </option>
-                        <option id="option--logout" className="profile-options" value="3">
-                        Favorites
-                        </option>
-                        <option id="option--logout" className="profile-options" value="4">
-                            Logout
-                        </option>
-                    </select>
-            </li>
-        </ul>
-    )
-    */
-
-    const [open, setOpen] = React.useState(false);
+       const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
 
   const handleToggle = () => {
@@ -124,7 +56,11 @@ export const NavBar = () => {
     prevOpen.current = open;
   }, [open]);
 
-  return (
+
+
+  const history= useHistory()
+
+    return (
       <>
       <article className="navbar">
           <section>
@@ -167,31 +103,45 @@ export const NavBar = () => {
                     aria-labelledby="composition-button"
                     onKeyDown={handleListKeyDown}
                   >
-                    <MenuItem>
-                      <ListItemIcon>
+                    <MenuItem onClick={()=>{
+                      history.push(`/`)
+                      handleToggle()
+                      }}>
+                      <ListItemIcon >
                         <HomeIcon fontSize="small" />
-                      </ListItemIcon>
-                      <ListItemText>Home</ListItemText>
+                      </ListItemIcon >
+                      <ListItemText >Home</ListItemText>
                     </MenuItem>
-                    <MenuItem>
+                    <MenuItem onClick={()=>{
+                      history.push(`/profile`)
+                      handleToggle()
+                      }}>
                       <ListItemIcon>
                         <PersonIcon fontSize="small" />
                       </ListItemIcon>
                       <ListItemText>Profile</ListItemText>
                     </MenuItem>
-                    <MenuItem>
+                    <MenuItem onClick={()=>{
+                      history.push(`/about`)
+                      handleToggle()
+                      }}>
                       <ListItemIcon>
                         <InfoIcon fontSize="small" />
                       </ListItemIcon>
                       <ListItemText>About</ListItemText>
                     </MenuItem>
-                    <MenuItem>
+                    <MenuItem onClick={()=>{
+                      history.push(`/favorites`)
+                      handleToggle()
+                      }}>
                       <ListItemIcon>
                         <StarIcon fontSize="small" />
                       </ListItemIcon>
                       <ListItemText>Favorites</ListItemText>
                     </MenuItem>
-                    <MenuItem>
+                    <MenuItem onClick={()=>{
+                      window.localStorage.removeItem("zen_user");
+                      history.push('/')}}>
                       <ListItemIcon>
                         <LogoutIcon fontSize="small" />
                       </ListItemIcon>
